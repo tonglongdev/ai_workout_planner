b9d6c4b7f4eb938866c90ef03b2b86a0c844368c
TongLongDev <tonglong.dev@gmail.com>
2026-04-01 17:31:39 +0700

    middleware check user in DB + handling token err

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { prisma } from "../lib/prisma";
 
 interface JwtPayload {
   userId: string;
+  email: string;
 }
 
 export interface AuthRequest extends Request {
   user?: JwtPayload;
 }
 
-export const authMiddleware = (
+export const authMiddleware = async (
   req: AuthRequest,
   res: Response,
   next: NextFunction,
@@ -28,10 +30,30 @@ export const authMiddleware = (
       process.env.JWT_SECRET as string,
     ) as JwtPayload;
 
-    req.user = decoded;
+    // check user in DB
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = {
+      userId: user.id,
+      email: user.email,
+    };
 
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: "Token expired" });
+    }
+
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
