87af57f589a64c6613bda16331c6692fd6d0a462
TongLongDev <tonglong.dev@gmail.com>
2026-04-01 16:48:26 +0700

    auth middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+  userId: string;
+}
+
+export interface AuthRequest extends Request {
+  user?: JwtPayload;
+}
+
+export const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+    ) as JwtPayload;
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
